d698cc6723b5cdfce85b5f4afab5bfffe43e6cac
Corneliu Iancu <corneliu.iancu27@gmail.com>
2026-02-15 13:35:23 +0200

    Initial neovim config
    Settings, three colorschemes (rose-pine, gruvbox, tokyonight),
    and commented keymaps ready to enable after vimtutor.

---
+++ init.lua
@@ -0,0 +1,127 @@
+-- Leader key (space) - prefix for custom shortcuts
+vim.g.mapleader = " "
+
+-- Bootstrap lazy.nvim (plugin manager)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git", "clone", "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugins
+require("lazy").setup({
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            require("rose-pine").setup({
+                disable_background = true,
+                styles = { italic = false },
+            })
+        end,
+    },
+    {
+        "ellisonleao/gruvbox.nvim",
+        name = "gruvbox",
+        config = function()
+            require("gruvbox").setup({
+                italic = { strings = false, emphasis = false, comments = false },
+            })
+        end,
+    },
+    {
+        "folke/tokyonight.nvim",
+        config = function()
+            require("tokyonight").setup({
+                style = "storm",
+                transparent = true,
+                styles = {
+                    comments = { italic = false },
+                    keywords = { italic = false },
+                },
+            })
+        end,
+    },
+})
+
+-- Set theme (change to "gruvbox" or "tokyonight" to switch)
+vim.cmd.colorscheme("rose-pine")
+
+-- Line numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- Indentation (4 spaces)
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- No line wrapping
+vim.opt.wrap = false
+
+-- No swap/backup files, persistent undo instead
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- Search: highlight as you type, clear when done
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Full color support (needed for themes)
+vim.opt.termguicolors = true
+
+-- Keep cursor 8 lines from screen edge when scrolling
+vim.opt.scrolloff = 8
+
+-- Always show sign column (prevents layout jumping)
+vim.opt.signcolumn = "yes"
+
+-- Faster update time
+vim.opt.updatetime = 50
+
+-- Vertical guide at column 80
+vim.opt.colorcolumn = "80"
+
+-- Keymaps to uncomment once you're comfortable with vimtutor:
+--
+-- Open file explorer
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+--
+-- Move selected lines up/down in visual mode
+-- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+-- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+--
+-- Half-page jump but keep cursor centered
+-- vim.keymap.set("n", "<C-d>", "<C-d>zz")
+-- vim.keymap.set("n", "<C-u>", "<C-u>zz")
+--
+-- Search next/prev but keep cursor centered
+-- vim.keymap.set("n", "n", "nzzzv")
+-- vim.keymap.set("n", "N", "Nzzzv")
+--
+-- Join lines without cursor jumping
+-- vim.keymap.set("n", "J", "mzJ`z")
+--
+-- Paste over selection without losing clipboard
+-- vim.keymap.set("x", "<leader>p", [["_dP]])
+--
+-- Yank to system clipboard
+-- vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+-- vim.keymap.set("n", "<leader>Y", [["+Y]])
+--
+-- Delete to void register (doesn't clobber clipboard)
+-- vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
+--
+-- Disable Ex mode (accidental key trap)
+-- vim.keymap.set("n", "Q", "<nop>")
+--
+-- Search and replace word under cursor
+-- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
